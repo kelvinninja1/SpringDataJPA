44894ed3c43072a7bd9867f752b6fdcd7ea6e773
Kelvin Morrison <kelvinninja1@gmail.com>
2021-09-15 01:57:09 +0000

    Writing from Application to the Database
    * Along with denerated persitent DB

--- src/main/java/io/turntabl/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/io/turntabl/springdatajpa/SpringDataJpaApplication.java
@@ -1,13 +1,32 @@
 package io.turntabl.springdatajpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDataJpaApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run(EmployeeRepository repository){
+        return (args -> {
+            insertJavaAdvocates(repository);
+            System.out.println(repository.findAll());
+        });
+    }
+
+    private void insertJavaAdvocates(EmployeeRepository repository){
+        repository.save(new Employee("Kelvin", "Obeng Morrison"));
+        repository.save(new Employee("Delta", "Aba Sheasha"));
+        repository.save(new Employee("Trisha", "See"));
+        repository.save(new Employee("Audrey", "Mengue"));
+        repository.save(new Employee("David", "Asiamah"));
+
     }
 
 }
